9856db6d41cfe69492705248ae446640359ba9f6
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-27 22:12:37 +0100

    setup 2

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// index.js ahora
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const { dbConnection } = require('./config/config');
+
+app.use(express.json());
+
+app.use('/', routes);
+
+
 
 dbConnection();
 
--- routes/task.js
+++ routes/task.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersRoutes = require('./users');
 
-router.use('/', usersRoutes);
+router.use('/', taskRoutes);
 
 module.exports = router;
